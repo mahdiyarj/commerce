f66a75c3fa760237368cc9735f93511a6c89ddad
Mahdiyar <mahdiyar.jadidiyan@gmail.com>
2025-01-08 12:10:34 +0330

    Initial data models

--- sales/models.py
+++ sales/models.py
@@ -1,3 +1,39 @@
+from django.contrib.auth.models import AbstractUser
+from django.core.validators import MinValueValidator
 from django.db import models
 
-# Create your models here.
+
+class User(AbstractUser):
+    email = models.EmailField(unique=True)
+
+
+class Product(models.Model):
+    title = models.CharField(max_length=255)
+    slug = models.SlugField()
+    description = models.TextField(null=True, blank=True)
+    price = models.DecimalField(
+        max_digits=12, decimal_places=0, validators=[MinValueValidator(1)]
+    )
+    inventory = models.IntegerField(validators=[MinValueValidator(0)])
+    last_update = models.DateTimeField(auto_now=True)
+
+
+class Invoice(models.Model):
+    user = models.ForeignKey(User, on_delete=models.PROTECT)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+
+class InvoiceItem(models.Model):
+    invoice = models.ForeignKey(Invoice, on_delete=models.PROTECT, related_name="items")
+    product = models.ForeignKey(
+        Product, on_delete=models.PROTECT, related_name="invoiceitems"
+    )
+    quantity = models.PositiveSmallIntegerField()
+    unit_price = models.DecimalField(max_digits=12, decimal_places=0)
+
+
+class Transaction(models.Model):
+    user = models.ForeignKey(User, on_delete=models.PROTECT)
+    invoice = models.ForeignKey(Invoice, on_delete=models.PROTECT)
+    created_at = models.DateTimeField(auto_now_add=True)
+    amount = models.DecimalField(max_digits=12, decimal_places=0)
